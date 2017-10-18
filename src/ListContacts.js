5519916e5a8d10856b0de81f1ca293a1d6ffb1c4
Guilherme Taschetto <gtaschetto@gmail.com>
2017-10-18 00:15:47 -0200

    Filter contacts by search term Also destructure props and state for better performance

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import escapeRegExp from 'escape-string-regexp'
+import sortBy from 'sort-by'
 
 class ListContacts extends Component {
   static propTypes = {
@@ -16,18 +18,31 @@ class ListContacts extends Component {
   }
 
   render() {
+    const { contacts, onDeleteContact } = this.props
+    const { query } = this.state
+
+    let showingContacts
+    if (query) {
+      const match = new RegExp(escapeRegExp(query), 'i')
+      showingContacts = contacts.filter(contact => match.test(contact.name))
+    } else {
+      showingContacts = contacts
+    }
+
+    showingContacts.sort(sortBy('name'))
+
     return (
       <div className='list-contacts'>
         <div className='list-contacts-top'>
           <input className='search-contacts'
             type='text'
             placeholder='Search contacts'
-            value={this.state.query}
+            value={query}
             onChange={event => this.updateQuery(event.target.value)}
           />
         </div>
         <ol className='contact-list'>
-          {this.props.contacts.map(contact => (
+          {showingContacts.map(contact => (
             <li key={contact.id} className='contact-list-item'>
               <div className='contact-avatar' style={{
                   backgroundImage: `url(${contact.avatarURL})`
@@ -37,7 +52,7 @@ class ListContacts extends Component {
                 <p>{contact.email}</p>
               </div>
               <button
-                onClick={() => this.props.onDeleteContact(contact)}
+                onClick={() => onDeleteContact(contact)}
                 className='contact-remove'>
                 Remove
               </button>
