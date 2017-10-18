9179261d71c055e02d084fd655290b5fbe66e193
Guilherme Taschetto <gtaschetto@gmail.com>
2017-10-18 00:59:59 -0200

    Use a remote API for contact storage

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -17,6 +17,9 @@ class ListContacts extends Component {
     this.setState({ query: query.trim() })
   }
 
+  clearQuery = () =>
+    this.setState({ query: '' })
+
   render() {
     const { contacts, onDeleteContact } = this.props
     const { query } = this.state
@@ -41,6 +44,14 @@ class ListContacts extends Component {
             onChange={event => this.updateQuery(event.target.value)}
           />
         </div>
+
+        {showingContacts.length !== contacts.length && (
+          <div className='showing-contacts'>
+            <span>Now showing {showingContacts.length} of {contacts.length} total</span>
+            <button onClick={this.clearQuery}>Show all</button>
+          </div>
+        )}
+
         <ol className='contact-list'>
           {showingContacts.map(contact => (
             <li key={contact.id} className='contact-list-item'>
