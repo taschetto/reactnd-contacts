ffd6faedcb5f9a330dc5f7d0c3e391642edfe810
Guilherme Taschetto <gtaschetto@gmail.com>
2017-10-17 22:21:59 -0200

    Render the users within the contact list

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -4,7 +4,11 @@ class ListContacts extends Component {
   render() {
     return (
       <ol className='contact-list'>
-        
+        {this.props.contacts.map(contact => (
+          <li key={contact.id}>
+            {contact.name}
+          </li>
+        ))}
       </ol>
     )
   }
