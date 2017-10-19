a61a4835c9fb6f143522796f536a6f54ebabf565
Guilherme Taschetto <gtaschetto@gmail.com>
2017-10-19 00:08:08 -0200

    Add a way to create contacts

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
+import { Route } from 'react-router-dom'
 import ListContacts from './ListContacts'
 import CreateContact from './CreateContact'
 import * as ContactsAPI from './utils/ContactsAPI'
 
 class App extends Component {
   state = {
-    screen: 'list', // list, create
     contacts: []
   }
 
@@ -23,20 +23,30 @@ class App extends Component {
     ContactsAPI.remove(contact)
   }
 
+  createContact(contact) {
+    ContactsAPI.create(contact).then(contact => {
+      this.setState(state => ({
+        contacts: this.state.contacts.concat([ contact ])
+      }))
+    })
+  }
+
   render() {
     return (
       <div className='app'>
-        {this.state.screen === 'list' && (
+        <Route exact path='/' render={() => (
           <ListContacts
             contacts={this.state.contacts}
             onDeleteContact={this.removeContact}
-            onNavigate={() => {
-              this.setState({ screen: 'create' })
-            }} />
-        )}
-        {this.state.screen === 'create' && (
-          <CreateContact/>
-        )}
+          />
+        )}/>
+        <Route path='/create' render={({ history }) => (
+          <CreateContact
+            onCreateContact={(contact) => {
+              this.createContact(contact)
+              history.push('/')
+            }}/>
+        )}/>
       </div>
     )
   }
